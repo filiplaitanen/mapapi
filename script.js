b2feeacabddd79e43f68dc1a023b48e0d5852800
Vilmer Niemi <75931083+Vilmer-niemi@users.noreply.github.com>
2024-09-10 13:51:57 +0200

    Pretty images

--- script.js
+++ script.js
@@ -20,8 +20,6 @@ function setAdress(adress) {
   });
 }
 
-setAdress('Berlin, Germany');
-
 async function SearchImage(){
     const response = await fetch("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=321f88dd6522f5ca711e9518babee1ac&tags=tree,water&format=json&nojsoncallback=1&has_geo=1");
 
