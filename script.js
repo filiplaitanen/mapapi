7fe48ce24f65ebe1ae8ea61c7e4159cbe291e6d6
Filip Laitanen <123742285+filiplaitanen@users.noreply.github.com>
2024-09-11 12:47:49 +0200

    map marker and better coords

--- script.js
+++ script.js
@@ -24,64 +24,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var geocoder = L.Control.Geocoder.nominatim()
 
-L.Control.MousePosition = L.Control.extend({
-
-	_pos: null,
-
-	options: {
-		position: 'bottomleft',
-		separator: ' : ',
-		emptyString: 'Unavailable',
-		lngFirst: false,
-		numDigits: 5,
-		lngFormatter: undefined,
-		latFormatter: undefined,
-		prefix: ""
-	},
-
-	onAdd: function (map) {
-		this._container = L.DomUtil.create('div', 'leaflet-control-mouseposition');
-		L.DomEvent.disableClickPropagation(this._container);
-		map.on('mousemove', this._onMouseMove, this);
-		this._container.innerHTML = this.options.emptyString;
-		return this._container;
-	},
-
-	onRemove: function (map) {
-		map.off('mousemove', this._onMouseMove)
-	},
-
-	getLatLng: function() {
-		return this._pos;
-	},
-
-	_onMouseMove: function (e) {
-		this._pos = e.latlng.wrap();
-		var lng = this.options.lngFormatter ? this.options.lngFormatter(e.latlng.wrap().lng) : L.Util.formatNum(e.latlng.wrap().lng, this.options.numDigits);
-		var lat = this.options.latFormatter ? this.options.latFormatter(e.latlng.lat) : L.Util.formatNum(e.latlng.lat, this.options.numDigits);
-		var value = this.options.lngFirst ? lng + this.options.separator + lat : lat + this.options.separator + lng;
-		this._container.innerHTML = this.options.prefix + ' ' + value;
-	}
-
-});
-
-L.Map.mergeOptions({
-	positionControl: false
-});
-
-L.Map.addInitHook(function () {
-	if (this.options.positionControl) {
-		this.positionControl = new L.Control.MousePosition();
-		this.addControl(this.positionControl);
-	}
-});
-
-L.control.mousePosition = function (options) {
-	return new L.Control.MousePosition(options);
-};
-
-L.control.mousePosition().addTo(geomap);
-
 //#endregion leaflet.js
 //#region our functions
 function setAdress(adress) {
@@ -126,7 +68,7 @@ function Search(){
 }
 
 async function onMapClick(e) {
-    const [lat, lng] = Object.values(geomap.getCenter())
+    const [lat, lng] = [e.latlng.lat, e.latlng.lng]
     const imgs = await SearchByCoordinates(lat, lng);
     if (imgs.length == 0){
         return;
@@ -137,7 +79,33 @@ async function onMapClick(e) {
 }
 
 // event listeners
-mapElement.addEventListener('dblclick', onMapClick);
+/*
+var [lattempd, lngtempd] = [0,0];
+var [lattempu, lngtempu] = [0,0];
+mapElement.addEventListener('mousedown', (e) => {
+    [lattempd, lngtempd] = Object.values(geomap.getCenter());
+});
+
+var marker;
+
+mapElement.addEventListener('mouseup', (e) => {
+    [lattempu, lngtempu] = Object.values(geomap.getCenter());
+    if( lattempu == lattempd && lngtempu == lngtempd ){
+        try{geomap.removeLayer(marker);} catch {}
+        marker = new L.marker([lattempu,lngtempu]).addTo(geomap)
+        geomap.addLayer(marker)
+        console.log(L.control.mousePosition())
+        onMapClick();
+    }
+});
+*/
+geomap.on('click', function(e){
+    alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
+    try{geomap.removeLayer(marker);} catch {}
+    marker = new L.marker([e.latlng.lat, e.latlng.lng]).addTo(geomap)
+    geomap.addLayer(marker)
+    onMapClick(e);
+});
 
 textInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter' && textInput.value != "") {
