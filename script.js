bbaebb1b4a9ea2dd3b188549fca99a56cf8b584d
Vilmer Niemi <75931083+Vilmer-niemi@users.noreply.github.com>
2024-09-11 14:08:30 +0200

    added satelite

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const textInput = document.querySelector('#input');
 const mapElement = document.querySelector('#map');
 const imageElement = document.querySelector('#image-container');
+const key = "321f88dd6522f5ca711e9518babee1ac";
 
 const inspect = {
     image: document.querySelector('#inspect-panel .inspect-image-style'),
@@ -16,13 +17,27 @@ const templates = {
 const image_count = 10;
 
 //#region leaflet.js
-var geomap = L.map('map').setView([0, 0],13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(geomap);
+const basemaps = {
+    StreetView: L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
+    Satelite:  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'),
+  };
+
+var geomap = L.map('map'
+, {center: [0, 0], zoom: 4,
+    layers: basemaps.StreetView
+}
+);
+
+L.control.layers(basemaps).addTo(geomap);
+
+
+
 
 var geocoder = L.Control.Geocoder.nominatim()
+
+
+
 
 //#endregion leaflet.js
 //#region our functions
@@ -31,27 +46,27 @@ function setAdress(adress) {
     var r = results[0];
     if (r) {
         geomap.fitBounds(r.bbox);
-      L.marker(r.center).addTo(geomap)
-        .bindPopup(r.html || r.name)
-        .openPopup();
+    
+
     }
   });
 }
+setAdress("sweden");
 async function FetchFlickr(method, param){
-    const response = await fetch("https://www.flickr.com/services/rest/?method="+method+"&api_key=321f88dd6522f5ca711e9518babee1ac&format=json&nojsoncallback=1&"+param);
+    const response = await fetch(`https://www.flickr.com/services/rest/?method=${method}&api_key=${key}&format=json&nojsoncallback=1&${param}`);
     const data = await response.json(); 
     console.log(data);  
     return data;
 }
 
 async function SearchByCoordinates(lat, lon){
-    const fetched = await FetchFlickr("flickr.photos.search", "lat="+lat+"&lon="+lon+"&has_geo=1&media=photos&per_page=10");
+    const fetched = await FetchFlickr("flickr.photos.search", `lat=${lat}&lon=${lon}&has_geo=1&media=photos&per_page=10`);
     const photos = fetched["photos"]["photo"]
     return photos;
 }
 
 async function InsertImages(tags){
-    const fetched = await FetchFlickr("flickr.photos.search", "tags="+tags+"&has_geo=1&media=photos&per_page=10");
+    const fetched = await FetchFlickr("flickr.photos.search", `tags=${tags}&has_geo=1&media=photos&per_page=10`);
     console.log(fetched["photos"]["photo"])
     const photos = fetched["photos"]["photo"]
 
@@ -74,7 +89,7 @@ async function onMapClick(e) {
         return;
     }
     for(let i = 0; i < image_count; i++){
-        imageElement.children[i].src = "https://live.staticflickr.com/"+imgs[i]["server"]+"/"+imgs[i]["id"]+"_"+imgs[i]["secret"]+".jpg";
+        imageElement.children[i].src = `https://live.staticflickr.com/${imgs[i]["server"]}/${imgs[i]["id"]}_${imgs[i]["secret"]}.jpg`;
     }
 }
 
